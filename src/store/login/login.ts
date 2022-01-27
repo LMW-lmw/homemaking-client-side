d1ed038309ab8897bd50120a0c4823b5e6a16dad
coderlmw <lmwl0113@163.com>
2022-01-27 18:25:02 +0800

    fix: 修改登录错误提示

--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -6,6 +6,7 @@ import { login, getUserInfo, getMenus } from '@/service/login/login'
 import router from '@/router/index'
 import storage from '@/utils/storage'
 import { mapRouter, mapMenuPermission } from '@/utils/map-menu'
+import { ElMessage } from 'element-plus'
 const loginModule: Module<ILogin, IRootState> = {
   namespaced: true,
   state: {
@@ -18,22 +19,32 @@ const loginModule: Module<ILogin, IRootState> = {
     async accountLogin({ commit, dispatch }, payload: IAccount) {
       // 登录存储token
       const loginResult = await login(payload)
-      const { id, token } = loginResult
-      storage.setItem('token', token, false)
-      commit('changeToken', token)
-      // 保存初始化部门菜单和角色菜单
-      dispatch('getInitList', null, { root: true })
-      // 储存用户信息
-      const userInfo = await getUserInfo(id)
-      storage.setItem('userInfo', userInfo, false)
-      commit('changeUserInfo', userInfo)
-      // 储存用户权限菜单
-      // console.log(userInfo.id)
-      const menus = await getMenus(id)
-      storage.setItem('menus', menus, false)
-      commit('changeMenus', menus)
-      // 跳转
-      router.push('/main')
+      const { id } = loginResult
+      const token = loginResult.token
+      if (token) {
+        storage.setItem('token', token, false)
+        commit('changeToken', token)
+        // 保存初始化部门菜单和角色菜单
+        dispatch('getInitList', null, { root: true })
+        // 储存用户信息
+        const userInfo = await getUserInfo(id)
+        storage.setItem('userInfo', userInfo, false)
+        commit('changeUserInfo', userInfo)
+        // 储存用户权限菜单
+        // console.log(userInfo.id)
+        const menus = await getMenus(id)
+        storage.setItem('menus', menus, false)
+        commit('changeMenus', menus)
+        // 跳转
+        router.push('/main')
+      } else {
+        ElMessage.error({
+          message: '用户名或密码错误',
+          type: 'error',
+          showClose: true,
+          duration: 2000
+        })
+      }
     },
     init({ commit, dispatch }) {
       const token = storage.getItem('token', false)
